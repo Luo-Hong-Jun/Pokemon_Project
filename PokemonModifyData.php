3c6cab5f1b6240b3977224b02108e1d739ae2106
Luo-Hong-Jun <luo.hong.jun@students.jedlik.eu>
2025-05-31 23:07:28 +0200

    minor changes

--- PokemonModifyData.php
+++ PokemonModifyData.php
@@ -3,31 +3,45 @@
     <form method="POST" action="?todo=add" class="form" enctype="multipart/form-data">
       <div class="mb-3">
         <label for="name" class="form-label">Név</label>
-        <input type="text" class="form-control size" id="name" name="name">
+        <input type="text" class="form-control size" id="name" name="name" required>
       </div>
       <div class="mb-3">
         <label for="type" class="form-label">típus</label>
-        <input type="text" class="form-control size" id="type" name="type">
+        <input type="text" class="form-control size" id="type" name="type" required>
       </div>
       <div class="mb-3">
         <label for="skill1" class="form-label">skill 1</label>
-        <input type="text" class="form-control size" id="skill1" name="skill1">
+        <input type="text" class="form-control size" id="skill1" name="skill1" required>
       </div>
       <div class="mb-3">
         <label for="skill2" class="form-label">skill 2</label>
-        <input type="text" class="form-control size" id="skill2" name="skill2">
+        <input type="text" class="form-control size" id="skill2" name="skill2" required>
       </div>
       <div class="mb-3">
         <label for="skill3" class="form-label">skill 3</label>
-        <input type="text" class="form-control size" id="skill3" name="skill3">
+        <input type="text" class="form-control size" id="skill3" name="skill3" required>
       </div>
       <div class="mb-3">
         <label for="skill4" class="form-label">skill 4</label>
-        <input type="text" class="form-control size" id="skill4" name="skill4">
+        <input type="text" class="form-control size" id="skill4" name="skill4" required>
       </div>
       <div class="mb-3">
+          <div class="form-check form-check-inline">
+            <label for="male" class="form-check-label">Hím</label>            
+            <input type="radio" class="form-check-input" id="male" name="gender" value="0" checked>
+          </div>
+          <div class="form-check form-check-inline">
+            <label for="female" class="form-check-label">Nőstény</label>
+            <input type="radio" class="form-check-input" id="female" name="gender" value="1">
+          </div>
+          <div class="form-check form-check-inline">
+            <label for="unknown" class="form-check-label">Ismeretlen</label>
+            <input type="radio" class="form-check-input" id="unknown" name="gender" value="2">
+          </div>
+      </div>
+      <div class="mb-3" style="margin: auto;">
         <label for="img" class="form-label">kép</label>
-        <input type="file" class="size" name="img" id="img" accept="image/png, image/gif, image/jpeg" />
+        <input type="file" class="size" name="img" id="img" accept="image/png, image/gif, image/jpeg" required/>
       </div>
       <button type="submit" class="btn btn-primary">Felvesz</button>
     </form>
--- PokemonShowData.php
+++ PokemonShowData.php
@@ -3,25 +3,18 @@
     $id = $_GET['id'];
     $solutions = new PokemonFuncs("localhost", "root", "", "pokemons");
     $pokemon = $solutions->GetPokemon($id);
-    echo "<h2 class='my-3 text-center'>$pokemon->name</h2>";
-        echo "<table class='table table-striped'>";
-    echo "<thead>";
-    echo "<tr><th>ID</th><th>Név</th><th>típus</th><th>skill1</th><th>skill2</th><th>skill3</th><th>skill4</th><th>gender</th><th>Törlés</th></tr>";
-    echo "</thead>";
-    echo "<tbody>";
-        echo "<tr>
-        <td></td>
-        <td></td>
-            <img src='$pokemon->img' width='300px'>
-            <td>$pokemon->type</td>
-            <td>$pokemon->skill1</td>
-            <td>$pokemon->skill2</td>
-            <td>$pokemon->skill3</td>
-            <td>$pokemon->skill4</td>
-            <td>$pokemon->gender</td>
-            <td><a href='?todo=del&id=$pokemon->id'><button>Törlés</button></a></td>
-        </tr>";
-    echo "</tbody>";
-    echo "</table>";
-    echo "<a href='index.php'><button>Kilépés</button></a>";
+        echo "      <div class='row'> 
+                        <div class='col-9'>
+                            <div style='height: 50vh;'>
+                                <img class='mx-auto d-block imgSize' src='$pokemon->img' alt='$pokemon->img'>
+                            </div>
+                            <div style='height: 49vh;'>
+                            </div>
+                        </div>
+                        <div class='col-3'>
+                            <a href='?todo=del&id=$pokemon->id'><button>Törlés</button></a></td>
+                            <a href='index.php'><button>Kilépés</button></a>;
+                        </div>
+                    </div>";
 ?>
+<div ></div>
