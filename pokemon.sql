8a03e7ab71994daf7a7f2b977d10da791c0097af
Luo-Hong-Jun <luo.hong.jun@students.jedlik.eu>
2025-06-01 21:29:39 +0200

    sql update base data

--- pokemon.sql
+++ pokemon.sql
@@ -15,7 +15,9 @@ CREATE TABLE pokemons.pokemon_table (
   PRIMARY KEY (id)
 )
 ENGINE = INNODB,
-AUTO_INCREMENT = 17,
+AUTO_INCREMENT = 56,
 AVG_ROW_LENGTH = 3276,
 CHARACTER SET utf8mb4,
 COLLATE utf8mb4_hungarian_ci;
+
+INSERT INTO `pokemon_table` (`id`, `name`, `type`, `img`, `skill1`, `skill2`, `skill3`, `skill4`, `gender`) VALUES (NULL, 'Vaporeon', 'Water', 'uploads/134.png', 'water gun', 'hydra pump', 'bite', 'freeze', '1'), (NULL, 'geodude', 'dsa', 'uploads/375px-0074Geodude.png', 'dddd', 'ssss', 'aaa', 'AAA', '0'), (NULL, 'diglett', 'ground', 'uploads/050.png', 'earthquake', 'rock slash', 'throw up', 'punch', '0'), (NULL, 'magnemite', 'dsadsa', 'uploads/081.png', 'sadasd', 'sadasd', 'sdadsadasd', 'dasdasda', '2'), (NULL, 'Gyattrados', 'water flying', 'uploads/130.png', 'water gun', 'freeze', 'fly', 'flamethrower', '0')
